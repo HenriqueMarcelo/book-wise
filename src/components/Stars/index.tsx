08d6b116ae563f63ad7270205ff2b2dc27a634d0
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-21 09:52:39 -0300

    feat: add param size to Stars component

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -1,15 +1,36 @@
 import { Star, StarHalf } from 'phosphor-react'
 import { Container } from './styles'
 
-export function Stars() {
+interface StarProps {
+  size?: 'sm' | 'md' | 'lg'
+}
+
+export function Stars({ size = 'sm' }: StarProps) {
   const rating = 3.5
+  let iconSize = 14
+
+  switch (size) {
+    case 'sm':
+      iconSize = 14
+      break
+    case 'md':
+      iconSize = 18
+      break
+    case 'lg':
+      iconSize = 24
+      break
+    default:
+      iconSize = 14
+      break
+  }
+
   return (
     <Container>
       {Array.from({ length: 5 }).map((_, i) => {
         return rating < i + 1 && rating > i ? (
-          <StarHalf size={14} weight="fill" />
+          <StarHalf size={iconSize} weight="fill" />
         ) : (
-          <Star size={14} weight={rating >= i + 1 ? 'fill' : undefined} />
+          <Star size={iconSize} weight={rating >= i + 1 ? 'fill' : undefined} />
         )
       })}
     </Container>
